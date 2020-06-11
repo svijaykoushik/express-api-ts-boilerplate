cb72f880d9206453f4600e4398b67e0d4a47ede3
Vijaykoushik Sekar <vijaykoushik@blaze.ws>
2020-06-11 22:47:46 +0530

    more eslint rules and fixes

--- src/app/index.ts
+++ src/app/index.ts
@@ -6,15 +6,12 @@ import { AddressInfo } from 'net';
 import { Server } from 'http';
 export class App {
     private app: express.Express;
-    constructor() {
+
+    public constructor() {
         this.app = express();
         this.init();
     }
-    private init() {
-        this.app.use(helmet());
-        this.app.use(cors());
-        this.app.use(bodyParser.json({ limit: '50mb' }));
-    }
+    
     public listen(): Server {
         const server = this.app.listen(3000, 'localhost',() => {
             const address = (server.address() as AddressInfo).address;
@@ -22,5 +19,11 @@ export class App {
             console.log(`Application running on url 📡: ${address}:${port}/`);
         });
         return server;
+    }
+    
+    private init() {
+        this.app.use(helmet());
+        this.app.use(cors());
+        this.app.use(bodyParser.json({ limit: '50mb' }));
     }
 }
