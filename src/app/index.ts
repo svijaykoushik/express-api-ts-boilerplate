8f7ab46785a62ef7c83217d7664fcaa94c3da2b2
Vijaykoushik Sekar <vijaykoushik@blaze.ws>
2020-06-11 22:47:21 +0530

    es lint auto fixes

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
 import { AddressInfo } from 'net';
 import { Server } from 'http';
 export class App {
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { App } from "./app";
+import { App } from './app';
 
 class Server {
     public static main(): void {
