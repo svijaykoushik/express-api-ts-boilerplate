bd83fa9e153c7f1ac7cd0405c41b5fd16fb83813
Vijaykoushik, S <svijaykoushik@hotmail.com>
2024-12-22 23:37:00 +0530

    feat: Add containerization support
    Adds support for containerizing the app using docker

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,62 @@
-import { existsSync } from 'fs';
+import { accessSync, constants, existsSync } from 'fs';
 import { config } from 'dotenv';
-import { join } from 'path';
+import { dirname, join } from 'path';
 import { exit } from 'process';
 
 // Load the configuration as soon as possible
 
 if (existsSync(join(__dirname, '../.env'))) {
     config({ path: join(__dirname, '../.env') });
-} else if (existsSync(join(__dirname, '../.env.example'))) {
+}
+
+if (
+    !process.env.APP_PORT ||
+    !process.env.APP_URL ||
+    !process.env.TYPEORM_DATABASE ||
+    !process.env.TYPEORM_LOG_QUERY ||
+    !process.env.SWAGGER_PORT ||
+    !process.env.SWAGGER_DOMAIN
+) {
+    process.env.APP_PORT = process.env.APP_PORT || (5050).toString();
+    process.env.APP_URL = process.env.APP_URL || '0.0.0.0';
+    process.env.TYPEORM_DATABASE =
+        process.env.TYPEORM_DATABASE || 'play_ground.db';
+    process.env.TYPEORM_LOG_QUERY =
+        process.env.TYPEORM_LOG_QUERY || false.toString();
+    process.env.SWAGGER_PORT = process.env.SWAGGER_PORT || (5050).toString();
+    process.env.SWAGGER_DOMAIN = process.env.SWAGGER_DOMAIN || '0.0.0.0';
+
     console.log(
         '❗ Caution: Default environment variables in use. ' +
             'Create a .env file and customize configurations ' +
             'for optimal security and tailored functionality.\n'
     );
-    config({ path: join(__dirname, '../.env.example') });
-} else {
-    console.error(
-        '❌ Failed start application\n',
-        'Unable to load configuration'
-    );
-    exit(1);
 }
+console.log('process.env.TYPEORM_DATABASE',process.env.TYPEORM_DATABASE);
+
+try {
+    accessSync(dirname(process.env.TYPEORM_DATABASE), constants.R_OK | constants.W_OK);
+    console.log('can read/write',dirname(process.env.TYPEORM_DATABASE));
+  } catch (err) {
+    console.error('no access!',dirname(process.env.TYPEORM_DATABASE));
+  }
+
+// if (existsSync(join(__dirname, '../.env'))) {
+//     config({ path: join(__dirname, '../.env') });
+// } else if (existsSync(join(__dirname, '../.env.example'))) {
+//     console.log(
+//         '❗ Caution: Default environment variables in use. ' +
+//             'Create a .env file and customize configurations ' +
+//             'for optimal security and tailored functionality.\n'
+//     );
+//     config({ path: join(__dirname, '../.env.example') });
+// } else {
+//     console.error(
+//         '❌ Failed start application\n',
+//         'Unable to load configuration'
+//     );
+//     exit(1);
+// }
 
 import { App } from './app';
 import getDataSource from './app/config/db-config';
