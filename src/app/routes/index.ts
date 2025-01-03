df58c760744d8a3bb98654d4b8ae7aace3b1b4e8
Vijaykoushik, S <svijaykoushik@hotmail.com>
2025-01-03 23:06:07 +0530

    chore: Remove unnecessary commented codes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -6,13 +6,6 @@ import { SampleController } from '../controllers/sample/sample-contoller';
 import { RootRouter } from './root-router';
 import { AuthRouter } from './auth';
 
-// export const routes = [
-//     new RootRouter(),
-//     new AuthRouter(),
-//     new SampleRouter(new SampleController()),
-//     new AppRouter(new AppController(new AppService()))
-// ];
-
 export const route = new RootRouter(
     new AuthRouter(),
     new SampleRouter(new SampleController()),
