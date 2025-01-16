abc37f577afab264d5e18c6817208a0c6466cb19
Vijaykoushik, S <svijaykoushik@hotmail.com>
2025-01-16 17:02:30 +0530

    test(app): Enhance app test suite with additional test cases
    Enhances the app test suite by adding additional test cases to improve
    test coverage and ensure comprehensive testing. These new tests
    cover App instantiation and Server Starting

--- src/app/index.spec.ts
+++ src/app/index.spec.ts
@@ -1,9 +1,34 @@
 import { expect } from 'chai';
+import sinon from 'sinon';
 import { App } from '.';
 
-describe('Application', function () {
+describe('App', function () {
+    let appInstance: App;
+
+    beforeEach(() => {
+        appInstance = new App();
+    });
+
+    afterEach(() => {
+        sinon.restore();
+    });
+
     it('should create an instance', function () {
-        const app = new App();
-        expect(app).to.be.an.instanceOf(App);
+        expect(appInstance).to.be.an.instanceOf(App);
+    });
+
+    it('should start server on specified port', function (done) {
+        const logSpy = sinon.spy(console, 'log');
+        const server = appInstance.listen();
+
+        setTimeout(() => {
+            expect(logSpy.calledWithMatch(/Application running on url 📡:/)).to
+                .be.true;
+            expect(
+                logSpy.calledWithMatch(/Api documentation running on url 📡:/)
+            ).to.be.true;
+            server.close();
+            done();
+        }, 50);
     });
 });
